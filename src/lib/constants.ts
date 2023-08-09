a8024f206fd2f990e532a6c7dfbd36a347e6b63a
RahulNavneeth <rahulmnavneeth@gmail.com>
2023-08-10 00:03:11 +0530

    master

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
 const TEXT_FILE_NAME = "PunIntended_PullRequest";
-export const FETCH_URL = `https://github.com/bitspaceorg/yuvenza-workshop/blob/main/${TEXT_FILE_NAME}.txt`;
+export const FETCH_URL = `https://github.com/bitspaceorg/yuvenza-workshop/blob/master/${TEXT_FILE_NAME}.txt`;
